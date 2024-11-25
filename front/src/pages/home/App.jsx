3331ae7006efeee594e8d167296ff04329cee0fc
guizindev <guilhermevasco2015@gmail.com>
2024-11-24 21:04:41 -0300

    basics done

--- front/src/pages/home/App.jsx
+++ front/src/pages/home/App.jsx
@@ -1,15 +1,31 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './style.css'
 import api from '../../services/api.js'
 
 function App() {
   const [users, setUsers] = useState([])
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  async function createUsers(){
+    await api.post('./users', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    })
+
+    getUsers()
+  }
 
   async function getUsers(){
     const usersFromApi = await api.get('/users')
-
     setUsers(usersFromApi.data)
-    console.log(usersFromApi.data)
+  }
+
+  async function deleteUser(id){
+    await api.delete(`/users/${id}`)
+    getUsers()
   }
 
   useEffect(() => {
@@ -21,10 +37,10 @@ function App() {
 
       <form action="submit">
         <h1>Cadastro de usuários</h1>
-        <input type="text" placeholder='Nome' name='name'/>
-        <input type="number" placeholder='Idade'name='age'/>
-        <input type="email" placeholder='Email' name='email '/>
-        <button type='button'>Cadastrar</button>
+        <input type="text" placeholder='Nome' name='name' ref={inputName} required/>
+        <input type="number" placeholder='Idade'name='age' ref={inputAge} required/>
+        <input type="email" placeholder='Email' name='email' ref={inputEmail} required/>
+        <button type='button' onClick={createUsers}>Cadastrar</button>
       </form>
 
       {users.map((users) => (
@@ -34,7 +50,7 @@ function App() {
            <p>Idade: <span>{users.age}</span></p>
            <p>Email: <span>{users.email}</span></p>
           </div>
-         <button>X</button>
+         <button onClick={() => deleteUser(users.id)}>X</button>
         </div>
       ))}
     </div>
